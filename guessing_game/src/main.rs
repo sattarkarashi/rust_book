35e627f14af193a48ab1ea65577549a3834aed72
sattarkarashi <satokarashi@gmail.com>
2024-07-20 00:31:08 +0200

    Added rand and cmp to the guessing game.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,10 +1,22 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Hello, this is a guessing game");
     println!("Enter your desired guess, please");
+
     let mut guess = String::new();
     io::stdin().read_line(&mut guess).expect("Failed to read line");
-    println!("Here is your guess: {guess}");
+    let guess: u32 = guess.trim().parse().expect("Enter a valid number");
+    let rand_value = rand::thread_rng().gen_range(1..=100);
+
+    match
+    guess.cmp(&rand_value){
+        Ordering::Less => println!("Too Small!"),
+        Ordering::Greater => println!("Too Big!"),
+        Ordering::Equal => println!("Nailed it!"),
+    }
+    println!("Here is your guess: {guess} and here is the guess value: {rand_value}");
 
 }
