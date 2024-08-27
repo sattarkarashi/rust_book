1188c8350bd75987e0b4f51e6c51b26c9929e156
sattarkarashi <satokarashi@gmail.com>
2024-08-27 22:36:36 +0200

    Error handling using match exressions and closures of Result

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,5 @@
 use std::fs::File;
+use std::io::ErrorKind;
 
 fn main() {
     // Error handling: we have two types of errors, recoverable and unrecoverable
@@ -13,6 +14,32 @@ fn main() {
 
     // Recoverable errors using Result: Some errors don't need to stop the program and crash like not finding a file in a location. So, you can by creating the file, remove the error.
 
-    let getting_file_result = File::open("hello.txt");
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result{
+        Ok(file) => file,
+        Err(error) => match error.kind(){
+            ErrorKind::NotFound => {
+                match File::create("hello.txt") {
+                    Ok(fc) => fc,
+                    Err(e) => panic!("Problem creating the file: {:?}", e),
+                }
+            }other_error => {
+                panic!("Problem opening the file: {:?}", other_error);
+
+            }
+        }
+    };
+
+    // An alternative to using match with Result<T,E> as a concise and cleaner way 
+    let greeting_file_result2 = File::open("hello2.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello2.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+    
 
 }
