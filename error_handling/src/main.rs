d4e116e0c97b4c64ca0fa3d844bf92d5de0ffd3e
sattarkarashi <satokarashi@gmail.com>
2024-08-28 22:18:40 +0200

    Propagating errors, ? operator, unrwap and expect errors added

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,5 +1,6 @@
-use std::fs::File;
+use std::fs::{self,File};
 use std::io::ErrorKind;
+use std::io::{self, Read};
 
 fn main() {
     // Error handling: we have two types of errors, recoverable and unrecoverable
@@ -40,6 +41,41 @@ fn main() {
             panic!("Problem opening the file: {:?}", error);
         }
     });
+
+    // unwrap expect
+
+    // let greeting_file3 = File::open("hello.txt").expect("hello.txt should be included in the this project.");
+
+    // Propagating error: create a function which reads a username from a file
+
+    fn read_username_from_file() -> Result <String, io::Error> {
+        let username_file_result = File::open("hello.txt");
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(e) => return Err(e)
+        };
+        let mut username = String::new();
+
+        match username_file.read_to_string(&mut username){
+            Ok(_) => Ok(username),
+            Err(e) => Err(e),
+        }
+    }
+
+    // The ? operator for propagating errors
+    fn read_username_from_file_with_operator() -> Result <String, io::Error> {
+        let mut username = String::new();
+
+        File::open("hello.txt")?.read_to_string(&mut username)?;
+        Ok(username)
+    }
+
+    // Let's make it even shorter using fs::read_to_string
+    fn read_username_from_file_with_fs() -> Result <String, io::Error> {
+
+        fs::read_to_string("hello.txt")
+    }
+
     
 
 }
