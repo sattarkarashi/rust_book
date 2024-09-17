62abe41d1e81a47dc64154e3059f433e1e50477c
sattarkarashi <satokarashi@gmail.com>
2024-09-17 21:40:17 +0200

    Added should_panic and custom messages inside it

--- test_pr/src/lib.rs
+++ test_pr/src/lib.rs
@@ -17,7 +17,20 @@ fn adds_two(a:i32) -> i32 {
 }
 
 pub fn greetings(name: &str) -> String {
-    format!("Hello!")
+    format!("Hello {name}!")
+}
+
+pub struct Guess {
+    value:i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 300 {
+            panic!("The value should be between 1 and 100 but got {}", value);
+        }
+        Guess{value}
+    }
 }
 
 #[cfg(test)]
@@ -64,5 +77,11 @@ mod tests {
         let result = greetings("Sato");
 
         assert!(result.contains("Sato"),"Greetings didn't contain name, the value was {result}");
+    }
+
+    #[test]
+    #[should_panic(expected="less than or equal to 100")]
+    fn greater_than_100 (){
+        Guess::new(200);
     }
 }
