485e691b36ad9159d741af437e1b15cdfbebe010
sattarkarashi <satokarashi@gmail.com>
2024-10-11 21:39:10 +0200

    deref Trait, how box<T> is different from references and its structure

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -45,6 +45,40 @@ fn main() {
     assert_eq!(5,x);
     assert_eq!(5,*y);
 
+    // Let's build a Box<T> and show how is different from references
+
+    struct MyBox<T>(T);
+    impl<T> MyBox<T> {
+        fn new(x:T) -> MyBox<T>{
+            MyBox(x)
+        }
+    }
+
+    use std::ops::Deref;
+
+    impl<T> Deref for MyBox<T> {
+        type Target = T;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+
+    let x = 5;
+    let y = MyBox::new(x);
+    assert_eq!(5,x);
+    assert_eq!(5,*y);
+
+    // *y actually implements the *y.deref() in practice.
+
+    // Implicit Deref Coercion
+    fn hello(name: &str) {
+        println!("Hello, {name}!");
+    }
+
+    let m = MyBox::new(String::from("Sato"));
+    hello(&m);
+
+
 
 
 
