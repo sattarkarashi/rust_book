74ab5b60b9b7e746be2b489fdf5ec94ea0a15c67
sattarkarashi <satokarashi@gmail.com>
2024-10-18 21:30:15 +0200

    Creating reference cycles

--- pointers/src/main.rs
+++ pointers/src/main.rs
@@ -14,10 +14,25 @@ use std::cell::RefCell;
 
  // allowing multiple owners of mutable data with Rc and Refcell
 
- #[derive(Debug)]
+//  #[derive(Debug)]
+//  enum List {
+//      Cons(Rc<RefCell<i32>>, Rc<List>),
+//      Nil,
+//  }
+
+#[derive(Debug)]
  enum List {
-     Cons(Rc<RefCell<i32>>, Rc<List>),
-     Nil,
+    Cons(i32, RefCell<Rc<List>>),
+    Nil,
+ }
+
+ impl List {
+    fn tail(&self) -> Option<&RefCell<Rc<List>>> {
+        match self {
+            Cons(_, item) => Some(item),
+            Nil => None,
+        }
+    }
  }
 
  use crate::List::{Cons, Nil};
@@ -154,17 +169,38 @@ fn main() {
     // let x = 5;
     // let y = &mut x;
 
-    let value = Rc::new(RefCell::new(5));
-    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
-    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    // let value = Rc::new(RefCell::new(5));
+    // let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+    // let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
 
-    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+    // let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
 
-    *value.borrow_mut() += 10;
-    println!("a after = {:?}", a);
-    println!("b after = {:?}", b);
-    println!("c after = {:?}", c);
+    // *value.borrow_mut() += 10;
+    // println!("a after = {:?}", a);
+    // println!("b after = {:?}", b);
+    // println!("c after = {:?}", c);
 
+    // Creating reference cycles
+    
+    let a = Rc::new(Cons(5, RefCell::new(Rc::new(Nil))));
+    println!("a initial rc count = {}", Rc::strong_count(&a));
+    println!("a next item = {:?}", a.tail());
+
+    let b = Rc::new(Cons(10, RefCell::new(Rc::clone(&a))));
+    println!("a rc count after b creation = {}", Rc::strong_count(&a));
+    println!("b next item = {:?}", b.tail());
+
+    if let Some(link) = a.tail() {
+        *link.borrow_mut() = Rc::clone(&b);
+    }
+    println!("b rc count after changing a = {}", Rc::strong_count(&b));
+    println!("a rc count after changing a = {}", Rc::strong_count(&a));
+
+    // The following println will overflow the stack!
+    println!("a next item = {:?}", a.tail());
+
+
+    
 
 
  
