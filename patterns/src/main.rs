f024d8006371dbbaac808f5e65014dedc3378889
sattarkarashi <satokarashi@gmail.com>
2024-11-07 21:08:56 +0100

    while conditionals and if loops

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -9,34 +9,51 @@ fn main() {
     // }
 
 
-let some_value: Option<String> = Some(String::from("Sato"));
+    let some_value: Option<String> = Some(String::from("Sato"));
 
-match some_value {
-    Some(v) => println!("The value is {v}"),
-    None => println!("No value"),
-}
+    match some_value {
+        Some(v) => println!("The value is {v}"),
+        None => println!("No value"),
+    }
 
-// If let conditionals
-let favorite_color:Option<&str> = None;
-let is_tuesday = false;
+    // If let conditionals
+    let favorite_color:Option<&str> = None;
+    let is_tuesday = false;
 
-let age:Result<u8, _> = "34".parse();
+    let age:Result<u8, _> = "34".parse();
 
-if let Some(color) = favorite_color {
-    println!("Using your favorite, {color} as background.");
-}else if is_tuesday {
-    println!("Tuesday is a green day!");
-}else if let Ok(age) = age {
-    if age > 30 {
-        println!("Using purple as the background color");
+    if let Some(color) = favorite_color {
+        println!("Using your favorite, {color} as background.");
+    }else if is_tuesday {
+        println!("Tuesday is a green day!");
+    }else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        }else {
+            println!("Using orange as the background color");
+
+        }
+
     }else {
-        println!("Using orange as the background color");
+        println!("Using blue as the background color");
 
     }
 
-}else {
-    println!("Using blue as the background color");
+    // While let conditionals
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
 
-}
+    while let Some(top) = stack.pop() {
+        println!("{top}")
+    }
+
+    // for loops
+    let v = vec!["a", "b", "c"];
+
+    for (index, value) in v.iter().enumerate(){
+        println!("{value} is at index {index}");
+    }
 
 }
