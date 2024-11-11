d0f065e17ae6b07d98bd35c19d4bb7a0966122d3
sattarkarashi <satokarashi@gmail.com>
2024-11-11 21:23:04 +0100

    Named variables mathcing

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -85,5 +85,17 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+    // Matching named variables
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {y}",x);
 
 }
