a4e431e099eb967c1faaef1faa3a766829f49b25
sattarkarashi <satokarashi@gmail.com>
2024-11-15 22:13:53 +0100

    destructuring enums and nested structs and enums

--- patterns/src/main.rs
+++ patterns/src/main.rs
@@ -142,6 +142,50 @@ fn main() {
     }
 
 
-    
+    // Destructuring enums
 
+    enum Message {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(i32, i32, i32),
+    }
+
+    let msg = Message::ChangeColor(0, 160, 255);
+
+    match msg {
+        Message::Quit => {
+            println!("The Quit variant has no data to destructure.");
+        }
+        Message::Move { x, y } => {
+            println!("Move in the x direction {x} and in the y direction {y}");
+        }
+        Message::Write(text) => println!("Text message: {text}"),
+        Message::ChangeColor(r, g, b) => println!("Change the color to red {r}, green {g}, and blue {b}"),
+    }
+
+    // Destructuring nested tuples
+    enum Color {
+        Rgb(i32, i32, i32),
+        Hsv(i32, i32, i32),
+    }
+    
+    enum Message2 {
+        Quit,
+        Move { x: i32, y: i32 },
+        Write(String),
+        ChangeColor(Color),
+    }
+
+    let msg = Message2::ChangeColor(Color::Hsv(0, 160, 255));
+
+    match msg {
+        Message2::ChangeColor(Color::Rgb(r, g, b)) => {
+            println!("Change the color to red {r}, green {g}, and blue {b}");        
+        }
+        Message2::ChangeColor(Color::Hsv(h, s, v)) => {
+            println!("Change the color to hue {h}, saturation {s}, and value {v}");     
+        }
+        _ => (),
+    }
 }
